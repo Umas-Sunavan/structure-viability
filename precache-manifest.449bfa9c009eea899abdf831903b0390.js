5e4e247a8037cd9bf48cdc76898475153ca3c49c
Umas-Sunavan <30775199+Umas-Sunavan@users.noreply.github.com>
2019-09-25 15:59:59 +0800

    Updates

---
+++ precache-manifest.449bfa9c009eea899abdf831903b0390.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2036a6834e14db364a64fbd339240ab",
+    "url": "/structure-viability/index.html"
+  },
+  {
+    "revision": "d5db4e292050cdf08fb2",
+    "url": "/structure-viability/static/css/main.f39f3cc3.chunk.css"
+  },
+  {
+    "revision": "a92ef9781f6997f6f767",
+    "url": "/structure-viability/static/js/2.812a260a.chunk.js"
+  },
+  {
+    "revision": "d5db4e292050cdf08fb2",
+    "url": "/structure-viability/static/js/main.298402b6.chunk.js"
+  },
+  {
+    "revision": "5c41d22ace911421cd13",
+    "url": "/structure-viability/static/js/runtime-main.1c7a37e2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/structure-viability/static/media/logo.25bf045c.svg"
+  }
+]);
